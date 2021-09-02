b846d761c492c406f5e479c350eaf3472e6a6b52
arex123 <ad47kumar@gmail.com>
2021-09-02 17:06:35 +0530

    mongodb is all set

---
+++ credenceanalytics backend asgnmt/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URI: `mongodb+srv://new_userad:asgnmt2backend@cluster0.alhg4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+}
--- credenceanalytics backend asgnmt/server.js
+++ credenceanalytics backend asgnmt/server.js
@@ -2,6 +2,29 @@ const express = require('express');
 
 const app = express();
 
+//When the user reaches http://localhost:5000/ they will be able to access posts_controller.js file which has all the routes.
+app.use(express.json());
+
+//add mongo to server
+
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./config');
+
+//When the user reaches http://localhost:5000/ they will be able to access posts_controller.js file which has all the routes.
+const postsRoutes = require('./routes/api/posts_controller')
+
+
+mongoose.connect(MONGO_URI,{
+    useNewUrlParser: true,
+    useUndefinedTopology: true
+}).then(() => console.log('MongoDB connected!'))
+.catch(err =>console.log(err));
+
+
+//When the user reaches http://localhost:5000/ they will be able to access posts_controller.js file which has all the routes.
+app.use('/api/posts', postsRoutes);
+
+
 app.get('/',(req,res) =>{
     res.send('Hello World');
 });
